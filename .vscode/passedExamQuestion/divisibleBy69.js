a070b8da7865dae3e97fd35120bca3328d3e66dc
DHIRAJ KR <107358694+Dhirajkr08@users.noreply.github.com>
2024-06-09 03:22:57 +0530

    program to check whether number is divisible by 69 or not

---
+++ .vscode/passedExamQuestion/divisibleBy69.js
@@ -0,0 +1,12 @@
+function isDivisibleBy(num){
+    if(num===69){
+        return 0
+
+    }
+    let ans=num%69
+    let missingNumber=69-ans
+    return missingNumber
+
+}
+let n=69
+console.log(isDivisibleBy(n))
